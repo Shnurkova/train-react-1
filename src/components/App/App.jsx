7d722726c5ef0caef6e6451e017cfb2d69b80af4
Shnurkova <Annshnur@gmail.com>
2024-05-28 21:54:40 +0300

    add transactions

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,16 @@ import Profile from "../Profile/Profile"
 import userData from "../../userData.json"
 import FriendList from "../FriendList/FriendList";
 import friends from "../../friends.json";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+import transactions from "../../transactions.json";
 
 export default function App() {
   
   return (
     <>
       <Profile user={userData} />
-      <FriendList friends={friends}/>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
     </>
   );
 }
