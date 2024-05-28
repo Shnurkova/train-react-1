e099e0371b1a2af998c36a878582182b7b34e7b2
Shnurkova <Annshnur@gmail.com>
2024-05-28 21:25:56 +0300

    add friendlist

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,12 +1,13 @@
 import FriendListItem from "../FriendListItem/FriendListItem";
+import css from "./FriendList.module.css"
 
 export default function FriendList({ friends }) {
     return (
         <>
-            <ul>
+            <ul className={css.list}>
                 {friends.map(friend => (
-                <li key={friend.id}>
-                    <FriendListItem friend={ friends } />
+                <li className={css.listitem} key={friend.id}>
+                    <FriendListItem friend={ friend } />
                 </li>
                 ))}
             </ul>
